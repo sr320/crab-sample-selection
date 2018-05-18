7e3bb53b5042ff603326bd598e6b2aea6a6d4541
Grace Crandall <graceac9@uw.edu>
2018-05-18 12:18:50 -0700

    added left_join to create new master with all qPCR data from Pam and my RNA isolation results

--- scripts/sample-selection.R
+++ scripts/sample-selection.R
@@ -1,32 +1,37 @@
+library(dplyr)
+
+qpcr <- read.csv("https://raw.githubusercontent.com/RobertsLab/project-crab/master/data/20180517-all-runs-qPCR-results.csv")
+
+RNA <- read.csv("https://raw.githubusercontent.com/RobertsLab/project-crab/master/data/goodsamples.csv")
+
+master <- left_join(RNA, qpcr, by = "FRP")
+
 #A more detailed look at experiment with qPCR data included
 
-crabdata_pcr <- read.csv("https://raw.githubusercontent.com/RobertsLab/project-crab/master/data/20180517_sequence_samples.csv")
-
-crabdata_pcr
 
 install.packages("tidyverse")
 
 
 library(tidyverse)
 
-crabdata_pcr
+master
 
 #listing column name
-colnames(crabdata_pcr)
+colnames(master)
 
-crabdata_pcr %>% 
-  select(FRP, Sample_Day, temperature_treatment, infection_status, sq_mean_d01, sq_mean_d26) %>% 
-  arrange(infection_status,sq_mean_d01)
+master %>% 
+  select(FRP, Sample_Day, temperature_treatment, infection_status, SQ_Mean, SQ_Std_Dev) %>% 
+  arrange(infection_status,SQ_Mean)
 
 
-pcrsum <- crabdata_pcr %>% 
-  select(FRP, infection_status, sq_mean_d01, sq_mean_d26) %>% 
-  arrange(infection_status,sq_mean_d01) %>%
+pcrsum <- master %>% 
+  select(FRP, infection_status, SQ_Mean) %>% 
+  arrange(infection_status,SQ_Mean) %>%
   unique()
 
 
 
-
+#The following may not be possible becuase I have not rearranged the data to be in separate columns based on sample day. How can I rearrange them in R? 
 
 ggplot(data = pcrsum) + 
   geom_histogram(aes(x = sq_mean_d01)) +
